0f3f431debb474f748e33a0207e892c63b2a56f3
Vlad <aniskovec.vlad@mail.ru>
2023-01-24 19:22:58 +0300

    initial commit

---
+++ server/main.js
@@ -0,0 +1,50 @@
+import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
+import { TasksCollection } from '/imports/db/TasksCollection';
+import '/imports/api/tasksMethods';
+import '/imports/api/tasksPublications';
+
+const insertTask = (taskText, user) =>
+  TasksCollection.insert({
+    text: taskText,
+    userId: user._id,
+    createdAt: new Date(),
+  });
+
+const SEED_USERNAME = 'Vlad';
+const SEED_PASSWORD = 'password';
+
+Meteor.startup(() => {
+  if (!Accounts.findUserByUsername(SEED_USERNAME)) {
+    Accounts.createUser({
+      username: SEED_USERNAME,
+      password: SEED_PASSWORD,
+    });
+  }
+
+  const user = Accounts.findUserByUsername(SEED_USERNAME);
+
+  if (TasksCollection.find().count() === 0) {
+    [
+      'First Task',
+      'Second Task',
+      'Third Task',
+      'Fourth Task',
+      'Fifth Task',
+      'Sixth Task',
+      'Seventh Task',
+    ].forEach(taskText => insertTask(taskText, user));
+  }
+
+  ServiceConfiguration.configurations.upsert(
+    { service: 'github' },
+    {
+      $set: {
+        loginStyle: 'popup',
+        clientId: 'fc6524eaaa72e18856b7',
+        secret: '7610316e2560013343ec1d8480f77325b9de3e35', 
+      },
+    }
+  );
+
+});
